4572828d59c1d5ccd7a8e09ee5149eab9c8b6aae
Aditya Singh <a23aditya@gmail.com>
2026-01-27 22:37:07 +0530

    .

---
+++ classMemberfun.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Rec{
+	public:
+		void set_dimension(int l,int b);
+
+		int para(){
+			return 2*length*width;	
+		}
+	private:
+		int length,width;
+};
+
+
+void Rec::set_dimension(int l,int b)
+{
+	length = l;
+	width = b;
+}
+
+int main(){
+	Rec r;
+	r.set_dimension(2,5);
+	cout<<r.para();
+};
