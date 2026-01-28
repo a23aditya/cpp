5c9a08ed52a8ed6e91de9d7c336c08e1af4f8439
Aditya Singh <a23aditya@gmail.com>
2026-01-28 22:52:31 +0530

    .

--- ofstream.cpp
+++ ofstream.cpp
@@ -3,11 +3,24 @@ using namespace std;
 
 #include<fstream>
 
+
+//-------------------------------------------------------------------------
+/*
+| Mode          | Meaning                    | When to use                |
+| ------------- | -------------------------- | -------------------------- |
+| `ios::out`    | Open for writing (default) | Normal file write          |
+| `ios::app`    | Append mode                | Logs, audit trails         |
+| `ios::trunc`  | Truncate file              | Overwrite old content      |
+| `ios::ate`    | Start at end (can seek)    | Modify file end            |
+| `ios::binary` | Binary mode                | Images, firmware, raw data |
+| `ios::in`     | Read mode                  | Rare with `ofstream`       |
+*/
+//-------------------------------------------------------------------------
 int main(){
 
 	ofstream of;
 
-	of.open("abc.txt");
+	of.open("abc.txt",ofstream::app);
 	
 	if(of.fail())
 	{
