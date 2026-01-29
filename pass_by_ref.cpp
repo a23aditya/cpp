cc9fc68452a55f317096c9e34905e0b6ad28ad5a
Aditya Singh <a23aditya@gmail.com>
2026-01-29 22:49:30 +0530

    .

--- pass_by_ref.cpp
+++ pass_by_ref.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
 using namespace std;
 
+class ABC{
+
+	public:
+	int count;
+
+	ABC(){
+		count =0;
+	}	
+
+	void increment()
+	{
+		++count;
+	}
+
+	void print_count(){
+		cout<<"count is now "<<count;
+	}
+};
+
+void increment_3x(ABC &abc)
+{
+	abc.increment();
+	abc.increment();
+	abc.increment();
+}
+
+
 //pass by ref
 void fun(int &a)
 {
@@ -12,6 +39,12 @@ int main(){
 	int a =5;
 	fun(a);
 
-	cout<<"a is now "<<a;
+	cout<<"a is now "<<a<<endl;
+
+	ABC c;
+	
+	increment_3x(c);
+	
+	c.print_count();
 
 };
