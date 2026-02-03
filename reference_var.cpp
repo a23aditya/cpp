25e080d0a9dfb67c9cd822b0e13d3e25bef27047
Aditya Singh <a23aditya@gmail.com>
2026-02-04 00:23:16 +0530

    .

--- reference_var.cpp
+++ reference_var.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void fun(int &a)
+int fun(int &a)
 {
 	++a;
+	return a;
 }
+
+int (&plusOne)(int&) = fun;
 
 int main(){
 	int a= 8;
@@ -17,4 +20,8 @@ int main(){
 
 	
 	cout << "a is "<< a << "b is "<<b<<endl;
+
+	plusOne(a);
+	cout<<"------"<<endl;
+	cout<<b<<endl;
 };
