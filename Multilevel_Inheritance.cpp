50a436fbdc51859661dca26d8569927609eed465
Aditya Singh <a23aditya@gmail.com>
2026-02-05 23:50:52 +0530

    .

---
+++ Multilevel_Inheritance.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class A{
+	public:
+	int data;
+
+	A(){
+		cout<<"----A-----"<<endl;
+		data = 1;
+		cout<<"----------"<<endl;
+	}
+
+	void show()
+	{
+		cout<< data<<endl;
+	}
+	~A()
+	{
+		cout<<"'++++++ A"<<endl;
+	}
+};
+
+
+class B : public A
+{
+	public:
+	B()
+	{
+		cout<<"--B---"<<endl;
+		data = 2;
+		cout<<"------"<<endl;
+	}
+	~B()
+	{
+		cout<<"'++++++ B"<<endl;
+	}
+};
+
+class C : public B
+{
+	public:
+	C()
+	{
+		cout<< "----C----"<<endl;
+		data = 3;
+		cout<<data;	
+		cout<<"---------"<<endl;
+	}
+	~C()
+	{
+		cout<<"'++++++ C"<<endl;
+	}
+};
+
+int main(){
+	
+	C o1;
+	
+//	o1.show();	
+	
+};
