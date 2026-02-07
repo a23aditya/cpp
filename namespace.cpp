fae13084df50b40951eb5151a232d060c9109243
Aditya Singh <a23aditya@gmail.com>
2026-02-07 14:36:37 +0530

    .

---
+++ namespace.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+namespace raw
+{
+	void print()
+	{
+		cout<<"raw A"<<endl;
+	}
+}
+
+namespace oos
+{
+        void print()
+        {
+                cout<<"oos B"<<endl;
+        }
+}
+
+
+int main(){
+
+	raw::print();
+
+	oos::print();
+
+};
